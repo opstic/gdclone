a74006204b9a607de32f4848c112145fb732a9ef
Opstic <46141527+opstic@users.noreply.github.com>
2023-04-06 12:47:57 -0400

    Leave console enabled for now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin};
 use bevy::prelude::*;
